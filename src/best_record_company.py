a9db9b225a3c9af0e433d94769e850ec6ea0c25e
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-11-05 13:18:23 -0500

    dsone

--- src/best_record_company.py
+++ src/best_record_company.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def best_record_company():
-    pass
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    print(df)
+    df1= df.groupby("Publisher")["WoC"].sum()
+    publisher = df1.idxmax()
+    mask = df["Publisher"] == publisher
+    return df[mask]
 
 def main():
-    best_record_company()
+    print(best_record_company())
     
 
 if __name__ == "__main__":
